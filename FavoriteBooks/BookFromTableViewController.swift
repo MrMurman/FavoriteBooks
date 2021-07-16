148ee4c73cc52090331f6a0b26895f5a2082f52c
MrMurman <storm2150@yandex.ru>
2021-07-16 20:42:58 +0300

    Initial Commit

---
+++ FavoriteBooks/BookFromTableViewController.swift
@@ -0,0 +1,59 @@
+//
+//  BookFromTableViewController.swift
+//  FavoriteBooks
+//
+//  Created by Андрей Бородкин on 16.07.2021.
+//
+
+import UIKit
+
+class BookFromTableViewController: UITableViewController {
+
+    var book: Book?
+    
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var authorTextField: UITextField!
+    @IBOutlet var genreTextField: UITextField!
+    @IBOutlet var lengthTextField: UITextField!
+    
+    init?(coder: NSCoder, book: Book?) {
+        self.book = book
+        super.init(coder: coder)
+    }
+    
+    required init?(coder: NSCoder) {
+        self.book = nil
+        super.init(coder: coder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateView()
+        
+        let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        view.addGestureRecognizer(tap)
+    }
+    
+    func updateView() {
+        guard let book = book else {return}
+        
+        titleTextField.text = book.title
+        authorTextField.text = book.author
+        genreTextField.text = book.genre
+        lengthTextField.text = book.length
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let title = titleTextField.text,
+            let author = authorTextField.text,
+            let genre = genreTextField.text,
+            let length = lengthTextField.text else {return}
+        
+        book = Book(title: title, author: author, genre: genre, length: length)
+        performSegue(withIdentifier: "UnwindToBookTable", sender: self)
+    }
+    
+    
+
+    
+}
